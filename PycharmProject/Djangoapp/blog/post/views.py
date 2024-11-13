967db6e69390a832c724c562007de6b04cd342f3
swastikcoder <swastikr787@gmail.com>
2024-11-13 16:11:52 +0530

    new changes

--- PycharmProject/Djangoapp/blog/post/views.py
+++ PycharmProject/Djangoapp/blog/post/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from .models import Post
+
+def posts(request):
+    posts = Post.objects.all().order_by('date')
+    return render(request,'post/post_da.html', {'posts':posts})
