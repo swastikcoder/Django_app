b8b0563c741be73c4d53f3a81943d97439b13a93
swastikcoder <swastikr787@gmail.com>
2024-11-14 14:13:28 +0530

    dheeraj

--- PycharmProject/Djangoapp/blog/post/urls.py
+++ PycharmProject/Djangoapp/blog/post/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 #
 urlpatterns = [
-    path('', views.post_page)
+    path('', views.post_page),
+    path('<slug:slug>',views.post,name = 'post')
 ]
--- PycharmProject/Djangoapp/blog/post/views.py
+++ PycharmProject/Djangoapp/blog/post/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
+from django.http import HttpResponse
 
 def post_page(request):
     posts = Post.objects.all()
     return render(request,'post/posts.html', {"posts": posts})
+
+def post(request,slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'post/post.html', {'post': post})
+
+
 
