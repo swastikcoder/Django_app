0b0a5e1cb13349e145361a986235bc42e7cf2b07
swastikcoder <swastikr787@gmail.com>
2024-11-21 15:11:01 +0530

    loginleft

--- PycharmProject/Djangoapp/blog/post/urls.py
+++ PycharmProject/Djangoapp/blog/post/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 #
 urlpatterns = [
-    path('', views.post_page),
+    path('', views.post_page,name='posts'),
     path('<slug:slug>',views.post,name = 'post')
 ]
--- PycharmProject/Djangoapp/blog/users/urls.py
+++ PycharmProject/Djangoapp/blog/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('register/',views.user_register,name = "register")
+    path('',views.home,name='user'),
+    path('register/',views.user_register,name = "register"),
+    # path('login/',views.login_view, name = "login")
 ]
