df9e1b6c2d3e58a535dc65ce6449f3a316b97902
swastikcoder <swastikr787@gmail.com>
2024-11-25 14:28:12 +0530

    login form done authorization left

--- PycharmProject/Djangoapp/blog/users/urls.py
+++ PycharmProject/Djangoapp/blog/users/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.home,name='user'),
     path('register/',views.user_register,name = "register"),
-    # path('login/',views.login_view, name = "login")
+    path('login/',views.login_view, name = "login")
 ]
--- PycharmProject/Djangoapp/blog/users/views.py
+++ PycharmProject/Djangoapp/blog/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login
 
 
 def home(request):
@@ -10,8 +10,21 @@ def user_register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request,form.save())
             return redirect('/')
     else:
         form = UserCreationForm()
     return render(request,"users/user_register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            login(request,form.get_user())
+            return redirect('posts')
+
+
+
+    else:
+        form = AuthenticationForm()
+    return render(request,"users/user_login.html", {"form": form})
